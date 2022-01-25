53cb7921140b3fb87ea7474614718f6bbbc788ec
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-01-25 10:33:13 -0800

    region_spec: add attribute test for name
    References #9

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+  it "exists" do
+    Region.new
+  end
+
+  describe "attributes" do
+    it "has a name" do
+      region = Region.new
+      expect(region).to respond_to(:name)
+    end
+  end
+
 end
