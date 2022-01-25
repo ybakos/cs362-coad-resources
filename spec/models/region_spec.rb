01ce4f1c6cf2fd16a5936e064d7025f9fa5ee158
joeyst <joey@LAPTOP-I5NHUC49.localdomain>
2022-01-25 11:21:14 -0800

    Add method unit tests for region.rb
    references #10

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
-
+    describe 'methods' do
+        it 'returns its name from to_s' do
+            region = Region.new
+            region_name = 'hai'
+            region.name = region_name
+            expect(region.to_s).to eq(region_name)
+        end
+        
+        it 'creates region with name if it doesnt previously exist' do
+            region = Region.find_or_create_by(name: 'Unspecified')
+            expect(region.name).to eq('Unspecified')
+        end
+    end
 end
