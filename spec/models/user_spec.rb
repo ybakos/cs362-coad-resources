2b71bc3d00083e9535910dcbdfb92f2cff88c939
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-01-30 10:21:30 -0800

    user_spec: Add validation test for email presence
    References #18

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+    describe "validations" do
+        it "has an email" do
+            user = User.new(email: "r@s.org")
+            expect(user).to respond_to(:email)
+        end
+    end
+
 end
