7332200615d756cb392372d59f4435fbf7cb1855
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-01-30 10:28:27 -0800

    user_spec: Add test for blank name
    References #18

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe User, type: :model do
             user = User.new(email: "r@s.org")
             expect(user).to respond_to(:email)
         end
+
+        it "cannot have a blank name" do
+            user = User.new(email: "r@s.org", password: "lemonade")
+            expect(user).to be_valid
+            user.email = nil
+            expect(user).to_not be_valid
+            expect(user).to validate_presence_of(:email)
+        end
     end
 
 end
