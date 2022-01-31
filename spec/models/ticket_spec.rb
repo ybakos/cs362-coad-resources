6e4cdfbd20d896302b1252b4c4636dd286cf9f34
joeyst <joey@LAPTOP-I5NHUC49.localdomain>
2022-01-30 20:14:26 -0800

    Add tests for open? method under ticket_spec.rb

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
+    describe "methods" do
+        let (:ticket) { Ticket.new }
+        it "returns false from the open? method if :closed is true" do
+            ticket.closed = true
+            expect(ticket.open?).to eq(false)
+        end
 
+        it "returns true from the open? method if :closed is false" do
+            ticket.closed = true
+            expect(ticket.open?).to eq(false)
+            ticket.closed = false
+            expect(ticket.open?).to eq(true)
+        end
+    end
 end
