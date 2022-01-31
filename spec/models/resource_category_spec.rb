a7280247b4767abbd6e23b642bd93a777f00cd71
joeyst <joey@LAPTOP-I5NHUC49.localdomain>
2022-01-30 21:09:57 -0800

    Add resource_category method unit tests for self.unspecified, activate, and deactivate methods

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
+    describe "methods" do
+        let (:resource_category) { ResourceCategory.new }
+        it "returns ResourceCategory object with name as Unspecified" do
+            resource_category = ResourceCategory.unspecified
+            expect(resource_category.name).to eq('Unspecified')
+        end
+
+        it "sets active attribute to true when activate method is invoked" do
+            resource_category.active = false
+            expect(resource_category.active).to eq(false)
+            resource_category.activate
+            expect(resource_category.active).to eq(true)
+        end
+
+        it "sets active attribute to false when deactivate method is invoked" do
+            resource_category.active = true
+            expect(resource_category.active).to eq(true)
+            resource_category.deactivate
+            expect(resource_category.active).to eq(false)
+        end
+
+    end
 
 end
