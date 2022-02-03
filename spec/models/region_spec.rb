945301a21806ff585446bc7797691c79fb95046a
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-02-03 11:39:46 -0800

    region_spec: use factory to create object for has a name attribute test
    References #30

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -21,7 +21,6 @@ RSpec.describe Region, type: :model do
 
   describe "attributes" do
       it "has a name" do
-        region = Region.new
         expect(region).to respond_to(:name)
       end
   end
