2f6cbb2d5990da76f13a3ab27a7a29b1888fa647
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-02-04 11:50:12 -0800

    organizations: define organizations factory
    References #32

---
+++ spec/factories/organizations.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :organizations do
+    name { 'Fake Organization' },
+    email { 'FakeEmail@FakeEmail.com' },
+    phone { '+15413215678' },
+    status { 'approved' },
+    primary_name { 'FakeName' },
+    secondary_name { 'SecondaryFakeName' }, 
+    secondary_phone { '+15419874567' }
+  end
+end
