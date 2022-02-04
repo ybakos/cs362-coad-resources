2d38147b6c3404c3d053c73626e6f97ddb311719
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-02-04 09:57:12 -0800

    region_spec: begin adding factory object creation to unique name
    References #30

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -9,44 +9,44 @@ RSpec.describe Region, type: :model do
   end
 
   describe 'methods' do
-      it 'returns its name from to_s' do
-          expect(region.to_s).to eq("Fake Region")
-      end
+    it 'returns its name from to_s' do
+      expect(region.to_s).to eq(region.name)
+    end
       
-      it 'creates region with name if it doesnt previously exist' do
-          region = Region.find_or_create_by(name: 'Unspecified')
-          expect(region.name).to eq('Unspecified')
-      end
+    it 'creates region with name if it doesnt previously exist' do
+      region = Region.find_or_create_by(name: 'Unspecified')
+      expect(region.name).to eq('Unspecified')
+    end
   end
 
   describe "attributes" do
-      it "has a name" do
-        expect(region).to respond_to(:name)
-      end
+    it "has a name" do
+      expect(region).to respond_to(:name)
+    end
   end
   
   describe "validations" do
-      it "cannot have a blank name" do
-        expect(region).to be_valid
-        region.name = nil
-        expect(region).to_not be_valid
-        expect(region).to validate_presence_of(:name)
-      end
+    it "cannot have a blank name" do
+      expect(region).to be_valid
+      region.name = nil
+      expect(region).to_not be_valid
+      expect(region).to validate_presence_of(:name)
+    end
   
-      it "cannot have a length longer than 255" do
-        expect(region).to be_valid
-        region.name = "9" * 256
-        expect(region).to_not be_valid
-      end
+    it "cannot have a length longer than 255" do
+      expect(region).to be_valid
+      region.name = "9" * 256
+      expect(region).to_not be_valid
+    end
   
-      it "must have a unique name" do
-        duplicate_name = 'Fake Name'
-        region = Region.create!(name: duplicate_name)
-        duplicate_region = Region.new(name: 'Unique Name')
-        expect(duplicate_region).to be_valid
-        duplicate_region.name = duplicate_name
-        expect(duplicate_region).to be_invalid
-      end
+    it "must have a unique name" do
+      #region = Region.create!(name: duplicate_name)
+      #duplicate_region = Region.new(name: 'Unique Name')
+      duplicate_region = build(:region, name: 'Unique Name')
+      expect(duplicate_region).to be_valid
+      duplicate_region.name = region.name 
+      expect(duplicate_region).to be_invalid
+    end
   end
 
 end
