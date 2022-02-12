098c9c555d893cfbe272f3b4539c9596589ff2f1
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-02-11 16:52:59 -0800

    ticket_spec: remove redundent name length test
    References #37

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -42,20 +42,6 @@ RSpec.describe Ticket, type: :model do
     it { should validate_presence_of(:region_id) }
     it { should validate_presence_of(:resource_category_id) }
     
-    it "cannot have a blank name" do
-      region = Region.create!(name: "42")
-      resource_category = ResourceCategory.create!(name: "rc")
-      ticket.name = "valid"
-      ticket.phone = "+15417977899"
-      ticket.description = "345820"
-      ticket.region_id = region.id
-      ticket.resource_category_id = resource_category.id
-      expect(ticket).to be_valid
-      ticket.name = nil
-      expect(ticket).to_not be_valid
-      expect(ticket).to validate_presence_of(:name)
-    end
-
     it { should validate_length_of(:name).is_at_least(1).is_at_most(255) }
       
     it "has a valid phone number" do
