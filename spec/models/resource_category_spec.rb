5d380c766fe1d7317546de4171a9a4ca2cbadcd4
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-02-14 11:16:11 -0800

    resource_category_spec: cover active scope with test
    References #47

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -47,6 +47,19 @@ RSpec.describe ResourceCategory, type: :model do
     it { should have_many(:tickets) }
   end
 
+  describe "scopes" do
+    it "active" do
+      active_res_cat = create(:active_resource)
+      inactive_res_cat = create(:inactive_resource)
+      results = ResourceCategory.active
+      expect(results).to include(active_res_cat)
+      expect(results).to_not include(inactive_res_cat)
+    end
+
+    it "inactive" do
+    end
+  end
+
   describe "validations" do 
     let (:resource_category) { ResourceCategory.new }
     
