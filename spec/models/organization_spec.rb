09367fb54f7b0c53ddfae68dc3fe1a5702fd907d
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-02-14 12:08:48 -0800

    organization_spec: test uniqueness of validations
    References #50

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -22,6 +22,9 @@ RSpec.describe Organization, type: :model do
     it { should validate_length_of(:email).is_at_least(1).is_at_most(255) }
     it { should validate_length_of(:name).is_at_least(1).is_at_most(255) }
     it { should validate_length_of(:description).is_at_most(1020) }
+
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_uniqueness_of(:name).case_insensitive }
   end
 
   describe "attributes" do
