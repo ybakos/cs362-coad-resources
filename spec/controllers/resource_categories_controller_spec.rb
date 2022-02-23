ecf2f744fed7903b575da935c221cf78dd6cb5d1
joeyst <joeystenbeck@gmail.com>
2022-02-22 19:17:18 -0800

    resource_categories_controller_spec: Add unit tests for a non-logged in user

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -2,4 +2,35 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
 
+  describe "a non logged in user" do
+
+    it "redirects to the sign-in screen" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
+      get :edit, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+      get :show, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+
+      patch :activate, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+      patch :deactivate, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+      patch :update, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+
+      put :update, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+
+      post :create
+      expect(response).to redirect_to(new_user_session_path)
+
+      delete :destroy, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+  end
+
 end
