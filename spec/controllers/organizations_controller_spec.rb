12950de3528638f3ad2b64000483aa54461d123b
joeyst <joeystenbeck@gmail.com>
2022-02-24 07:36:02 -0800

    organizations_controller_spec: Add unit tests for non logged in user

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -2,4 +2,45 @@ require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
 
+  describe "a non logged in user" do
+
+    # these do not work
+    it "redirects them to the login screen" do
+
+      get :resources, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+
+      delete :destroy, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+
+    end
+
+    it "redirects them to the login screen" do
+
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
+      get :edit, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+      get :show, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+
+      post :approve, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+      post :reject, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+      post :create
+      expect(response).to redirect_to(new_user_session_path)
+
+      patch :update, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+
+      put :update, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+
+    end
+
+  end
+
 end
