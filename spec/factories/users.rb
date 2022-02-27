0d5d2ce01f738123755cbd86f2aa2bf39ee5772d
joeyst <joeystenbeck@gmail.com>
2022-02-27 10:30:23 -0800

    user factory: add :user_with_organization

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,18 @@
 FactoryBot.define do
   factory :user do
     email { 'test@test.com' }
-    password { 'test' }
+    password { 'testing' }
     
     factory :admin do
       role { admin }
+    end
+
+    factory :user_with_organization do
+      organization { create(:organization) }
+    end
+
+    factory :user_without_organization do
+      organization { nil }
     end
   end
 end
