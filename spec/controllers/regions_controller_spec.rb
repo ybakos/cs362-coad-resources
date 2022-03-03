c59c2c4ebd26a3b563ad00c1534e1eb78bf45f59
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-03-03 11:05:21 -0800

    regions_controller_spec: remove commented out code

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -16,6 +16,8 @@ RSpec.describe RegionsController, type: :controller do
       get :destroy, params: { id: 'FAKE' }
       expect(response).to redirect_to(new_user_session_path)
     end
+  end
+
   describe "an unapproved regions user" do
     it "redirects to the sign-in screen" do
       region_user = create(:user)
@@ -44,36 +46,4 @@ RSpec.describe RegionsController, type: :controller do
       expect(response).to redirect_to(dashboard_path)
     end
   end
-    
-
-    """
-
-    it 'redirects to the sign-in screen' do
-      get :index
-      expect(response).to redirect_to(new_user_session_path)
-      get :new
-      expect(response).to redirect_to(new_user_session_path)
-      get :edit, params: { id: 'FAKE' }
-      expect(response).to redirect_to(new_user_session_path)
-      get :show, params: { id: 'FAKE' }
-      expect(response).to redirect_to(new_user_session_path)
-
-      patch :activate, params: { id: 'FAKE' }
-      expect(response).to redirect_to(new_user_session_path)
-      patch :deactivate, params: { id: 'FAKE' }
-      expect(response).to redirect_to(new_user_session_path)
-      patch :update, params: { id: 'FAKE' }
-      expect(response).to redirect_to(new_user_session_path)
-
-      put :update, params: { id: 'FAKE' }
-      expect(response).to redirect_to(new_user_session_path)
-
-      post :create
-      expect(response).to redirect_to(new_user_session_path)
-
-      delete :destroy, params: { id: 'FAKE' }
-      expect(response).to redirect_to(new_user_session_path)
-    end
-    """
-  end 
 end
