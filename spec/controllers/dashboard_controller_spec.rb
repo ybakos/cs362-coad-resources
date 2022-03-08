4f4b7129321b832971625bb444a67fcb12a5d61d
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-03-08 10:31:12 -0800

    dashboard_controller_spec: cover user redirects to sign in screen
    References #62

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
 
+  describe "a user that is not logged in" do
+    it "redirects to the sign-in screen" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+    end 
+  end
 end
