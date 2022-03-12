f826387858fdac7ce02fe92dc8f8a425bbae2967
joeyst <joeystenbeck@gmail.com>
2022-03-11 17:02:49 -0800

    organizations_controller_spec: add unit tests for signed-in user without organization

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -34,9 +34,11 @@ RSpec.describe OrganizationsController, type: :controller do
 
   describe "a logged-in, non-admin user" do
 
-    let(:user) { create(:user_with_organization) }
+    let(:user) { create(:user) }
+    let(:organization) { create(:organization) }
 
     before do
+      user.organization = organization
       user.confirm
       sign_in(user)
       user.organization.approve
@@ -54,6 +56,35 @@ RSpec.describe OrganizationsController, type: :controller do
       post :reject, params: {id: 'FAKE'}
       expect(response).to redirect_to(dashboard_path)
 
+    end
+
+  end
+
+  describe "a logged-in user without an organization" do
+
+    let(:user) { create(:user) }
+
+    before do
+      user.confirm
+      sign_in(user)
+    end
+
+    it "redirects user without organization to dashboard" do
+
+      patch :update, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+      put :update, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+
+      get :edit, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+      get :show, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+
+      post :approve, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+      post :reject, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
 
     end
 
