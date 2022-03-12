0a90cc77a2f1ce83d75f98d4a963ff379906e4fc
joeyst <joeystenbeck@gmail.com>
2022-03-11 18:43:50 -0800

    organizations_controller_spec: fix spacing

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -1,11 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
-
   describe "a non logged in user" do
-
     it "redirects them to the login screen" do
-
       get :index
       expect(response).to redirect_to(new_user_session_path)
       get :new
@@ -24,16 +21,12 @@ RSpec.describe OrganizationsController, type: :controller do
 
       patch :update, params: {id: 'FAKE'}
       expect(response).to redirect_to(new_user_session_path)
-
       put :update, params: {id: 'FAKE'}
       expect(response).to redirect_to(new_user_session_path)
-
     end
-
   end
 
   describe "a logged-in, non-admin user" do
-
     let(:user) { create(:user) }
     let(:organization) { create(:organization) }
 
@@ -45,7 +38,6 @@ RSpec.describe OrganizationsController, type: :controller do
     end
 
     it "redirects user with organization to dashboard" do
-
       get :new
       expect(response).to redirect_to(dashboard_path)
       get :create
@@ -55,13 +47,10 @@ RSpec.describe OrganizationsController, type: :controller do
       expect(response).to redirect_to(dashboard_path)
       post :reject, params: {id: 'FAKE'}
       expect(response).to redirect_to(dashboard_path)
-
     end
-
   end
 
   describe "a logged-in user without an organization" do
-
     let(:user) { create(:user) }
 
     before do
@@ -70,7 +59,6 @@ RSpec.describe OrganizationsController, type: :controller do
     end
 
     it "redirects user without organization to dashboard" do
-
       patch :update, params: {id: 'FAKE'}
       expect(response).to redirect_to(dashboard_path)
       put :update, params: {id: 'FAKE'}
@@ -85,9 +73,6 @@ RSpec.describe OrganizationsController, type: :controller do
       expect(response).to redirect_to(dashboard_path)
       post :reject, params: {id: 'FAKE'}
       expect(response).to redirect_to(dashboard_path)
-
     end
-
   end
-
 end
