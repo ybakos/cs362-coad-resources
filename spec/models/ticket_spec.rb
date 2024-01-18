305c21fba3f554f673f63f818c03ef03e9e62f06
Curtis Kauer <kauercurtis@gmail.com>
2024-01-17 16:21:26 -0800

    2.2: instantiation Tests

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-
+    it 'exists' do
+        Ticket.new
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
+    it 'exists' do
+        User.new
+    end
 end
