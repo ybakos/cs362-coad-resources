2b88a02601698e470305398cb49eccc15f6a85ba
Curtis Kauer <kauercurtis@gmail.com>
2024-01-24 15:05:22 -0800

    3.1 validation testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,12 @@ RSpec.describe User, type: :model do
     describe "user" do
         it { should belong_to(:organization)}
     end
+
+    it {should validate_presence_of(:email)}
+    it {should validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create)}
+    it {should have_attribute(:email)}
+    it {should validate_uniqueness_of(:email).case_insensitive()}
+
+    it{should validate_presence_of(:password).on(:create)}
+    it{should validate_length_of(:password).is_at_least(7).is_at_most(255).on(:create)}
 end
