8ab4a6809678ce05c77a181e1cb02363feb6f70a
Curtis Kauer <kauercurtis@gmail.com>
2024-01-31 14:26:26 -0800

    tickets variables initialized by sequence

--- spec/factories/sequences.rb
+++ spec/factories/sequences.rb
@@ -15,5 +15,12 @@ FactoryBot.define do
     sequence :secondary_name do |n|
         "secondary_name_#{n}"
     end
-    
+
+    sequence :region_id do |n|
+        "fake_region_id_#{n}"
+    end
+
+    sequence :resource_category_id do |n|
+        "fake_resource_category_id_#{n}"
+    end
 end
--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     factory :ticket do
         #Changes not added, committed, pushed
         name
-        phone
+        phone {"1-555-123-5467"};
         region_id
         resource_category_id
     end
